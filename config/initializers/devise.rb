9ad0b02b9be6f44138dabb2f88e0e32187ba535c
Iaconi Gheorghe <iaconimgheorghe@gmail.com>
2020-09-25 23:19:19 +0200

    navbar and flash alerts

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV['GKEY']
+  config.mailer_sender = "make-it-done.com #{ENV['GNAME']}"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
