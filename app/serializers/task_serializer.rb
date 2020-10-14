24b13017d19d076ae3ea7cade70428a5dbcba384
Iaconi Gheorghe <iaconimgheorghe@gmail.com>
2020-10-14 22:14:56 +0200

    test user account in login page

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,4 +1,10 @@
 class TaskSerializer
   include FastJsonapi::ObjectSerializer
   attributes :description, :duration, :done, :list_id, :created_at
+
+  private 
+
+  def creation_date
+    Task.created_at.strftime("at %l%M %b/%d")
+  end
 end
